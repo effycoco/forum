c756039fdbc9cdbbc8562948f350c0cbb618b987
effycoco <734157786@qq.com>
2022-04-24 12:44:52 +0800

    chapter 2 completed

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,11 @@ module.exports = {
     "no-unused-vars": process.env.NODE_ENV === "production" ? "error" : "warn",
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    semi: "off",
+    quotes: "off",
+    "comma-dangle": "off",
+    "space-before-function-paren": "off",
   },
 
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/standard"],
+  extends: ["plugin:vue/vue3-essential", "@vue/standard"],
 };
