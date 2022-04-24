5f5a0aebc803d6f5f279098ef0e4f1216262ba63
effycoco <734157786@qq.com>
2022-04-24 21:10:12 +0800

    create ThreadList.vue

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,57 +1,20 @@
 <template>
   <!-- structure the contents of page-home component -->
-  <div v-for="thread in threads" :key="thread.id" class="col-large push-top">
-    <h1>{{ thread.title }}</h1>
-
-    <div class="post-list">
-      <div v-for="postId in thread.posts" :key="postId" class="post">
-        <div class="user-info">
-          <a href="#" class="user-name">{{
-            userById(postById(postId).userId).name
-          }}</a>
-
-          <a href="#">
-            <img
-              class="avatar-large"
-              :src="userById(postById(postId).userId).avatar"
-              alt=""
-            />
-          </a>
-
-          <p class="desktop-only text-small">107 posts</p>
-        </div>
-
-        <div class="post-content">
-          <div>
-            <p>{{ postById(postId).text }}</p>
-          </div>
-        </div>
-
-        <div class="post-date text-faded">
-          {{ postById(postId).publishedAt }}
-        </div>
-      </div>
-    </div>
-  </div>
+  <h1>Welcome to the Forum!</h1>
+  <ThreadList :threads="threads" />
 </template>
 
 <script>
+import ThreadList from "@/components/ThreadList.vue";
 import srcData from "@/data.json";
 export default {
+  components: {
+    ThreadList,
+  },
   data() {
     return {
       threads: srcData.threads,
-      posts: srcData.posts,
-      users: srcData.users,
     };
-  },
-  methods: {
-    postById(postId) {
-      return this.posts.find((post) => postId === post.id);
-    },
-    userById(userId) {
-      return this.users.find((user) => userId === user.id);
-    },
   },
 };
 </script>
