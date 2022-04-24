d544032aa23e64b55e882c8b790d31de9261a7b7
effycoco <734157786@qq.com>
2022-04-24 22:34:05 +0800

    redirect with vue router without change the url

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import PageHome from "@/components/PageHome.vue";
 import PageThreadShow from "@/components/PageThreadShow.vue";
 import NotFound from "@/components/PageNotFound.vue";
 import { createRouter, createWebHistory } from "vue-router";
+import srcData from "@/data.json";
 const routes = [
   {
     path: "/",
@@ -13,6 +14,27 @@ const routes = [
     name: "ThreadShow",
     component: PageThreadShow,
     props: true,
+    beforeEnter(to, from, next) {
+      // check if thread exits
+      const threadExits = srcData.threads.find(
+        (thread) => thread.id === to.params.id
+      );
+      // if it exits continue
+      if (threadExits) {
+        return next();
+      } else {
+        // if not redirect to not found
+        next({
+          name: "NotFound",
+          // break the active path to an array
+          // an pass it as the value of the pathMatch parameter to the NotFound page
+          params: { pathMatch: to.path.substring(1).split("/") },
+          // preserve existing query and hash
+          query: to.query,
+          path: to.path,
+        });
+      }
+    },
   },
   {
     path: "/:pathMatch(.*)*",
