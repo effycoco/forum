c443e4b60ca5cdbe97ebf9f244351cf2790857f8
effycoco <734157786@qq.com>
2022-04-25 16:10:50 +0800

    created a post editor

--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -3,6 +3,23 @@
   <div class="col-large push-top">
     <h1>{{ thread.title }}</h1>
     <post-list :posts="threadPosts"></post-list>
+    <div class="col-full">
+      <form @submit.prevent="addPost">
+        <div class="form-group">
+          <textarea
+            v-model="newPostText"
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            class="form-input"
+          />
+        </div>
+        <div class="form-actions">
+          <button class="btn-blue">Submit post</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -24,6 +41,7 @@ export default {
     return {
       threads: srcData.threads,
       posts: srcData.posts,
+      newPostText: "",
     };
   },
   computed: {
@@ -32,6 +50,22 @@ export default {
     },
     threadPosts() {
       return this.posts.filter((post) => post.threadId === this.id);
+    },
+  },
+  methods: {
+    //
+    addPost() {
+      const postId = "gggb" + Math.random();
+      const post = {
+        id: postId,
+        text: this.newPostText,
+        publishedAt: Math.floor(Date.now() / 1000),
+        threadId: this.id,
+        userId: "FsCDAk9w8NeXEceLV87arpsXjnQ2",
+      };
+      this.posts.push(post);
+      this.thread.posts.push(postId);
+      this.newPostText = "";
     },
   },
 };
